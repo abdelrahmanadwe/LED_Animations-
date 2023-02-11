51029dec6024ec4c526a6b1305c2d11b4eb44fe9
Abdelrahman Adwe Ali <117317854+abdelrahmanadwe@users.noreply.github.com>
2023-02-11 02:43:50 +0200

    Update main.c

--- 3-Shifting_Right_Every_250_ms/main.c
+++ 3-Shifting_Right_Every_250_ms/main.c
@@ -6,11 +6,11 @@
  */
 
 
-#include "C:\My-GitHub\AVR_ATMega32-Drivers\03-LIB\STD_TYPES.h"
-#include "C:\My-GitHub\AVR_ATMega32-Drivers\03-LIB\BIT_MATH.h"
+#include "..\AVR_ATMega32-Drivers\03-LIB\STD_TYPES.h"
+#include "..\AVR_ATMega32-Drivers\03-LIB\BIT_MATH.h"
 
-#include "C:\My-GitHub\AVR_ATMega32-Drivers\01-MCAL\01-DIO\DIO_interface.h"
-#include "C:\My-GitHub\AVR_ATMega32-Drivers\02-HAL\01-LED\LED_interface.h"
+#include "..\AVR_ATMega32-Drivers\01-MCAL\01-DIO\DIO_interface.h"
+#include "..\AVR_ATMega32-Drivers\02-HAL\01-LED\LED_interface.h"
 
 void main()
 {
